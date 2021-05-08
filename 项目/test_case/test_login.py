a6b899b45e04b6811672008f732f0c3362ba7c31
KevinWang886 <wangchangyu2010@hotmail.com>
2021-05-08 15:37:49 +0800

    更新test_login.py文件

--- 项目/test_case/test_login.py
+++ 项目/test_case/test_login.py
@@ -33,11 +33,10 @@ class TestLoin():
     @allure.story("登录")
     @allure.title("不输入账号密码登录")
     def test_login_without_account_and_pwd(self, login):
-        with allure.step("3、点击登录按钮"):
+        with allure.step("1、点击登录按钮"):
             login.click_login_btn()
             time.sleep(0.5)
-        with allure.step("4、断言"):
-            # print(LoginPage.get_account_error(login))
+        with allure.step("2、断言"):
             assert LoginPage.get_account_error(login) == \
                    Base.get_data(login, file_name='test_login.yaml')['test_login']['account_error']
             assert LoginPage.get_pwd_error(login) == \
@@ -47,12 +46,12 @@ class TestLoin():
     @allure.story("登录")
     @allure.title("不输入账号登录")
     def test_login_without_account(self, login):
-        with allure.step("2、输入密码"):
+        with allure.step("1、输入密码"):
             login.input_password(Base.get_data(login, file_name='test_login.yaml')['test_login']['password'])
-        with allure.step("3、点击登录按钮"):
+        with allure.step("2、点击登录按钮"):
             login.click_login_btn()
             time.sleep(0.5)
-        with allure.step("4、断言"):
+        with allure.step("3、断言"):
             # print(LoginPage.get_account_error(login))
             assert LoginPage.get_account_error(login) == \
                    Base.get_data(login, file_name='test_login.yaml')['test_login']['account_error']
@@ -61,12 +60,12 @@ class TestLoin():
     @allure.story("登录")
     @allure.title("不输入密码登录")
     def test_login_without_password(self, login):
-        with allure.step(Base.get_data(login, file_name='test_login.yaml')['test_login']['account']):
-            login.input_account("wcy")
-        with allure.step("3、点击登录按钮"):
+        with allure.step("1、输入账号"):
+            login.input_account(Base.get_data(login, file_name='test_login.yaml')['test_login']['account'])
+        with allure.step("2、点击登录按钮"):
             login.click_login_btn()
             time.sleep(0.5)
-        with allure.step("4、断言"):
+        with allure.step("3、断言"):
             # print(LoginPage.get_account_error(login))
             assert LoginPage.get_pwd_error(login) == \
                    Base.get_data(login, file_name='test_login.yaml')['test_login']['password_error']
@@ -75,10 +74,10 @@ class TestLoin():
     @allure.story("登录")
     @allure.title("输入错误账号密码登录")
     def test_login_with_wrong_account(self, login):
-        with allure.step(Base.get_data(login, file_name='test_login.yaml')['test_login']['incorrect_account']):
-            login.input_account(Base.get_data(login, file_name='test_login.yaml')['test_login']['incorrect_password'])
-        with allure.step("2、输入密码"):
-            login.input_password("123456")
+        with allure.step("1、输入错误账号"):
+            login.input_account(Base.get_data(login, file_name='test_login.yaml')['test_login']['incorrect_account'])
+        with allure.step("2、输入错误密码"):
+            login.input_password(Base.get_data(login, file_name='test_login.yaml')['test_login']['incorrect_password'])
         with allure.step("3、点击登录按钮"):
             login.click_login_btn()
             time.sleep(0.5)
