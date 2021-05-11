d4ac0c87414a25df787c45c80a31a660ac4537df
KevinWang886 <wangchangyu2010@hotmail.com>
2021-05-11 09:30:42 +0800

    更新base文件

--- 项目/base/base.py
+++ 项目/base/base.py
@@ -46,8 +46,7 @@ class Base():
             try:
                 ele = WebDriverWait(self.driver, self.timeout, self.t).until(EC.presence_of_element_located(locator))
             except TimeoutException as msg:
-                raise log.logger("定位元素出现超时！！！！，不要盯着报错看了，也不用截图贴群里了，"
-                                      "先把定位技术学好，别复制粘贴xpath, 请检查你的定位方式，在浏览器先调试成功，观察页面是否正常打开")
+                raise log.logger("定位元素出现超时！！！！")
             return ele
 
     def finds(self, locator):
